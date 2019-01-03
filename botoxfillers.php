62a8ddf8b840679783d510687a7d32e76557318e
aayushi kambariya <aayushi@pipabella.com>
2019-01-03 17:30:32 +0530

    Create botoxfillers.php

---
+++ botoxfillers.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Dental Studio 32</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <?php include 'load_resources.php';?>
+  </head>
+  <body>
+    <div class="main_container">
+        <!-- logo -->
+        <div class="banner">
+            <?php include 'navbar.php';?>
+            <div class="clear_aligners_bg">    
+                <div class="container services_info">
+                    <h3>  What is Botox and Fillers?</h3> 
+                    <ul>
+                        <li>
+                        BOTOX® is a purified protein complex produced by the clostridium botulinum bacterium.<br> Approved by the United States FDA, BOTOX is the world’s No.1 brand in cosmetic clinics, with over 11 million procedures since 2002. <br>
+                        </li>
+                        <li>BOTOX® for cosmetic indications is a simple, non-surgical, physician-administered <br>treatment that can temporarily smooth moderate to severe <br>
+                        Forehead Lines 
+                        ,Frown Lines 
+                        ,Crow’s Feet and 
+                        ,Bunny Lines.
+                        </li>
+                        
+                    </ul>
+                </div>
+            </div>
+    <!-- footer -->
+     <?php include 'footer.php';?>
+  </div>
+</body>
+</html>
+<style type="text/css">
+    .clear_aligners_bg
+    {
+        margin: 10%;
+        background-color: #f9fcff2e;
+        padding: 10px;
+        color: white;
+    }
+
+    .banner
+    {
+         background-image: url('img/fabian-albert-743193-unsplash.jpg');
+        background-position: center;
+        background-size: cover;
+        height:100%;
+        max-height:100%;
+    }
+</style>
